bdcaa8cba67fd2a13128e63188df25f91bc316a8
Denis <denis.yaremenko@huma.com>
2024-03-23 11:43:07 +0200

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Denis Yaremenko on 23.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
